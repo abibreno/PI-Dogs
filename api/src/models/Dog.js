7f39f5b7d0ee750f821c665e576c9c9623f26f9e
abibreno <jenniferabigailbreno@gmail.com>
2022-11-18 20:39:42 -0300

    modelos

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -8,7 +8,32 @@ module.exports = (sequelize) => {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    height: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    weight: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    life_span: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+  },
+    {
+      timestamps: true,
+      createdAt: 'creado',
+      updateAt: false,
   });
 }; 
 
-abigail
